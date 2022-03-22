3cadd973c751012a3a39263eeb77476715da3dba
Antsipau <antsipau@tut.by>
2022-03-22 17:56:30 +0300

    add new functions and test

--- base/seleniumbase.py
+++ base/seleniumbase.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,34 +9,53 @@ from typing import List
 class SeleniumBase:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 15, 0.3)
+        self.__wait = WebDriverWait(driver, 15, 0.3, ignored_exceptions=StaleElementReferenceException)
 
     def __get_selenium_by(self, find_by: str) -> dict:
+        '''Return a dictionary, where Keys are Strings representing a search locator strategies and Values are related By class values'''
         find_by = find_by.lower()
-        locating = {"css": By.CSS_SELECTOR,
-                    "Xpath": By.XPATH,
-                    "class_name": By.CLASS_NAME,
-                    "id": By.ID,
-                    "link_text": By.LINK_TEXT,
-                    "name": By.NAME,
-                    "partial_link_text": By.PARTIAL_LINK_TEXT,
-                    "tag_name": By.TAG_NAME}
+        locating = {'css': By.CSS_SELECTOR,
+                    'xpath': By.XPATH,
+                    'class_name': By.CLASS_NAME,
+                    'id': By.ID,
+                    'link_text': By.LINK_TEXT,
+                    'name': By.NAME,
+                    'partial_link_text': By.PARTIAL_LINK_TEXT,
+                    'tag_name': By.TAG_NAME}
         return locating[find_by]
 
     def is_visible(self, find_by: str, locator: str, locator_name: str = None) -> WebElement:
-        return self.wait.until(ec.visibility_of_element_located((self.__get_selenium_by(find_by), locator)), locator_name)
+        '''Waiting on element and return WebElement if it is visible'''
+        return self.__wait.until(ec.visibility_of_element_located((self.__get_selenium_by(find_by), locator)), locator_name)
 
     def is_present(self, find_by: str, locator: str, locator_name: str = None) -> WebElement:
-        return self.wait.until(ec.presence_of_element_located((self.__get_selenium_by(find_by), locator)), locator_name)
+        '''Waiting on element and return WebElement if it is present on DOM'''
+        return self.__wait.until(ec.presence_of_element_located((self.__get_selenium_by(find_by), locator)), locator_name)
 
     def is_not_present(self, find_by: str, locator: str, locator_name: str = None) -> WebElement:
-        return self.wait.until(ec.invisibility_of_element_located((self.__get_selenium_by(find_by), locator)),
-                               locator_name)
+        '''Wait on element until it disappears '''
+        return self.__wait.until(ec.invisibility_of_element_located((self.__get_selenium_by(find_by), locator)),
+                                 locator_name)
 
     def are_visible(self, find_by: str, locator: str, locator_name: str = None) -> List[WebElement]:
-        return self.wait.until(ec.visibility_of_all_elements_located((self.__get_selenium_by(find_by), locator)),
-                               locator_name)
+        '''Waiting on elements and return WebElements if they are visible'''
+        return self.__wait.until(ec.visibility_of_all_elements_located((self.__get_selenium_by(find_by), locator)),
+                                 locator_name)
 
     def are_present(self, find_by: str, locator: str, locator_name: str = None) -> List[WebElement]:
-        return self.wait.until(ec.presence_of_all_elements_located((self.__get_selenium_by(find_by), locator)),
-                               locator_name)
+        '''Waiting on elements and return WebElements if they are present on DOM'''
+        return self.__wait.until(ec.presence_of_all_elements_located((self.__get_selenium_by(find_by), locator)),
+                                 locator_name)
+
+    def get_text_from_webelements(self, elements: List[WebElement]) -> List[str]:
+        '''The input should be a list of WebElements, where we read text from each element and Return a List[String]'''
+        return [element.text for element in elements]
+
+    def get_element_by_text(self, elements: List[WebElement], name: str) -> WebElement:
+        '''The input should we a list of WebElements, from which we return a single WebElement found by it's name'''
+        name = name.lower()
+        return [element for element in elements if element.text.lower() == name][0]
+
+    def delete_cookie(self, cookie_name: str) -> None:
+        '''Delete a cookie by a name'''
+        self.driver.delete_cookie(cookie_name)
--- pom/homepage_nav.py
+++ pom/homepage_nav.py
@@ -1,6 +1,8 @@
-from base.seleniumbase import SeleniumBase
-from selenium.webdriver.remote.webelement import WebElement
 from typing import List
+
+from selenium.webdriver.remote.webelement import WebElement
+
+from base.seleniumbase import SeleniumBase
 
 
 class HomepageNav(SeleniumBase):
@@ -9,13 +11,20 @@ class HomepageNav(SeleniumBase):
         super().__init__(driver)
         self.driver = driver
         self.__nav_links: str = '#mainNavigationFobs>li'
-        self.NAV_LINK_TEXT = 'Own Your Style,Women,Men,Beauty,Home,Furniture,Kids,Toys,Shoes,Jewelry,Handbags,Sale,Gifts'
+        self.NAV_LINK_TEXT = 'Gifts,Women,Men,Kids & Baby,Beauty,Home,Furniture,Shoes,Jewelry,Handbags & Accessories,Now Trending,Sale'
 
     def get_nav_links(self) -> List[WebElement]:
+        '''Return WebElements for nav links'''
         return self.are_visible('css', self.__nav_links, 'Header Navigation Links')
 
     def get_nav_links_text(self) -> str:
+        '''Return all nav links text. Return format is a String with comma separated values'''
         nav_links = self.get_nav_links()
-        nav_links_text = [link.text for link in nav_links]
-        return ",".join(nav_links_text)
+        nav_links_text = self.get_text_from_webelements(nav_links)
+        return ','.join(nav_links_text)
+
+    def get_nav_link_by_name(self, name: str) -> WebElement:
+        '''Return a nav link WebElement, the input is a link's name'''
+        elements = self.get_nav_links()
+        return self.get_element_by_text(elements, name)
 
